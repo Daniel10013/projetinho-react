2d4f54bb4f9fc2f22723f9eeb2ff327ca98a540f
DanielFilipe100 <touristicmite10@gmail.com>
2022-10-31 20:28:55 -0300

    Finished the login endpoint | Created the create user

--- src/Controllers/UserController.ts
+++ src/Controllers/UserController.ts
@@ -2,46 +2,86 @@ import { Request, Response } from "express"
 import { PrismaClient } from "@prisma/client"
 import * as bcrypt from 'bcrypt'
 import { prismaDisconect, prismaError } from "../Libs/Prisma/PrimsaUtil"
+import { UserToCreate } from "../Types/userToCreateInterface"
 
 const prisma = new PrismaClient()
 
 class UserController{
-    async userLogin(req: Request, res: Response) {
-        try {
-          const data = req.body
-          let userToLogin: any
-          const login = async () => {
-            userToLogin = await prisma.usuarios.findFirst({
-              where: {
-                OR: {
-                  email: { equals: data.email },
-                },
-              },
-              select: {
-                senha: true,
-              },
-            })
-    
-            if (userToLogin == undefined) {
-              res.send(false)
-            } else {
-              // res.json(userToLogin)
-              (await bcrypt.compare(data.senha, userToLogin.senha))
-                ? res.send('true')
-                : res.send('false')
-            }
-          }
-          login()
-            .then(() => {
-              prismaDisconect()
-            })
-            .catch((e) => {
-              prismaError(e)
-            })
-        } catch (err) {
-          res.status(500).json(err)
+
+  // CREATE A NEW USER WITH A EMAIL EXISTANCE VERIFICATION
+  async createUser(req: Request, res: Response) {
+    try {
+      const data = req.body
+      const create = async () => {
+        const users: any = await prisma.usuarios.count({
+          where: {
+            email: data.email,
+          },
+        })
+        if (users > 0) {
+          res.send('Já existe uma conta com esse email!')
+          return
         }
-      }    
+        const userToCreate = await prisma.usuarios
+          .create({
+            data: {
+              email: data.email,
+              usuario: data.usuario,
+              cnpj: data.cnpj,
+              senha: await bcrypt.hash(req.body.senha, 15),
+            },
+          })
+          .then(() => {
+            res.status(201).send('Usuário criado com sucesso!')
+          })
+      }
+      create()
+        .then(() => {
+          prismaDisconect()
+        })
+        .catch((e) => {
+          prismaError(e)
+        })
+    } catch (err) {
+      res.status(400).json(err)
+    }
+  }
+
+  async userLogin(req: Request, res: Response) {
+    try {
+      const data = req.body
+      const login = async () => {
+        const userToLogin = await prisma.usuarios.findFirst({
+          where: {
+            OR: {
+              email: { equals: data.email },
+            },
+          },
+          select: {
+              senha: true,
+          }
+        })
+
+        if (userToLogin == undefined) {
+          res.send(false)
+          return
+        } 
+        
+        await bcrypt.compare(data.senha, userToLogin.senha) ? res.json('true') : res.send('false');
+
+      }
+      login()
+        .then(() => {
+            prismaDisconect()
+        })
+        .catch((e) => {
+          prismaError(e)
+        })
+    } 
+    catch (err) {
+      res.send(err)
+    }
+  }    
 }
 
 export default UserController
--- src/Routes/UserRoute.ts
+++ src/Routes/UserRoute.ts
@@ -1,8 +1,25 @@
+import UserController from '../Controllers/UserController'
 import express, {Request, Response} from 'express'
 
+const userController = new UserController()
+
 function routeUser(app: express.Application){
-    app.get('/api/v1/login',async (req: Request, res: Response) => {
-        res.send("Hello World");
+    app.post('/api/v1/users/login',async (req: Request, res: Response) => {
+        try {
+            await userController.userLogin(req, res)
+        } 
+        catch (err) {
+            console.log(err)
+        }
+    })
+
+    app.post('/api/v1/users/create',async (req: Request, res: Response) => {
+        try {
+            await userController.createUser(req, res)
+        } 
+        catch (err) {
+            console.log(err)
+        }
     })
 }
 
